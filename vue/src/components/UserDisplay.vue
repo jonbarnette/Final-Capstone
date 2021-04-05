de28f988635858b557b5bf56565ddb0b5896d393
Jon Barnette <me@jonbarnette.com>
2021-04-05 15:42:46 -0400

    Picture Flip

--- vue/src/components/UserDisplay.vue
+++ vue/src/components/UserDisplay.vue
@@ -32,14 +32,15 @@
         </div>
         <div class="divTableBody">
           <div class="divTableRow" v-for="cat in filteredCats" v-bind:key="cat.name">
-            <div class="divTableCell">
-                <img v-bind:src="cat.imageName" />
+            <div class="divTableImage">
+                <img class="image" v-bind:src="cat.imageName" />
             </div>
+            <div class="overlay">
             <div class="divTableCell">{{ cat.name }}</div>
             <div class="divTableCell">{{ cat.age }}</div>
             <div class="divTableCell">{{ cat.occupation }}</div>
             <div class="divTableCell">{{ cat.description }}</div>
-           
+           </div>
           </div>
         </div>
       </div>
@@ -87,5 +88,45 @@ data() {
 }
 .background {
   background-color: #16ffbd;
+}
+
+.divTableImage {
+  position: relative;
+  width: 50%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+}
+
+.container:hover .overlay {
+  opacity: 1;
+}
+
+.divTableCell {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
 }
 </style>
