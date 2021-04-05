34262a81d2323ad5e11519fbab376920d1912a5c
Charlie Nguyen <cnguyen1286@gmail.comm>
2021-04-05 15:06:55 -0400

    list is working

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -6,18 +6,24 @@
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       <router-link v-bind:to="{name:'cats'}">Cat List</router-link>
     </div>
+   
     <router-view />
+     <user-display />
     <user-list />
   </div>
 </template>
 
 <script>
 import UserList from "@/components/UserList.vue";
+import UserDisplay from "@/components/UserDisplay.vue"
+
 
 export default {
   components: {
 
-    UserList
+    UserList,
+    UserDisplay
+    
   },
   
     
