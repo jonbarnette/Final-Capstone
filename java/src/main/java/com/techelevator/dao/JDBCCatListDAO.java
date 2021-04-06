854052946e2c6c4f6a20662e59d95cfa77380777
Joeseph Brass <xenios239@gmail.com>
2021-04-06 13:17:16 -0400

    clicked saved

--- java/src/main/java/com/techelevator/dao/JDBCCatListDAO.java
+++ java/src/main/java/com/techelevator/dao/JDBCCatListDAO.java
@@ -8,13 +8,13 @@ public class JDBCCatListDAO implements CatListDAO {
 
 	@Override
 	public List<CatList> retrieveCatList() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public List<CatList> retrieveCatDetails(Long catId) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
