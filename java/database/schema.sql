7826f849df3f38d7d39b62165d0aa41035dedc70
Jon Barnette <me@jonbarnette.com>
2021-04-06 13:54:18 -0400

    update database schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -2,8 +2,16 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS seq_user_id;
+DROP TABLE IF EXISTS catlist;
+DROP SEQUENCE IF EXISTS seq_cat_id;
 
 CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_cat_id
   INCREMENT BY 1
   NO MAXVALUE
   NO MINVALUE
@@ -16,6 +24,20 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE catlist (
+	cat_id int DEFAULT nextval('seq_cat_id'::regclass) NOT NULL,
+	name varchar(50) NOT NULL,
+	age int NOT NULL,
+	breed varchar(50) NOT NULL,
+	color varchar(50) NOT NULL,
+	occupation varchar(50) NOT NULL,
+	tagline varchar(200) NOT NULL,
+	address varchar(200) NOT NULL,
+	summary varchar(1000) NOT NULL,
+	CONSTRAINT PK_catlist PRIMARY KEY (cat_id),
+	CONSTRAINT FK_catlist_users FOREIGN KEY (cat_id) REFERENCES users (user_id)
 );
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
