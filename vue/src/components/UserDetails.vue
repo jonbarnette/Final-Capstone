075424629c182420869a0f9ce2b44e2f2cd68811
Charlie Nguyen <cnguyen1286@gmail.comm>
2021-04-09 11:39:19 -0400

    details page showing everything

--- vue/src/components/UserDetails.vue
+++ vue/src/components/UserDetails.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="userdetail">
-  
-    <div class="placeFrequented">
+    
+    <div id="userBio" v-for="cat in catsArray" v-bind:key="cat.catId">
+      <div>
+        <img class="image" v-bind:src="getImageURL(cat.imageName)" />
+      </div>
+      <div>Name: {{ cat.name }}</div>
+      <div>Lives: {{ cat.lives }}</div>
+      <div>Breed: {{ cat.breed }}</div>
+      <div>Color: {{ cat.color }}</div>
+      <div>Occupation: {{ cat.occupation }}</div>
+    </div>
+
+    <div
+      class="placeFrequented"
+      v-for="cat in catsArray"
+      v-bind:key="cat.catId"
+    >
       <h3>List Of Places Frequented</h3>
 
-      <p>Places placeholder</p>
+      <p>{{ cat.address }}</p>
     </div>
-  <div class="summaryDetail">
+    <div class="summaryDetail" v-for="cat in catsArray" v-bind:key="cat.catId">
       <h3>Cat Summary</h3>
+      <p>{{ cat.summary }}</p>
     </div>
-    <div id="userBio" v-for="cat in catsArray" v-bind:key="cat.catId">
-       <div>Name:{{ cat.name }}</div> 
-
-      <div>Lives</div>
-      <div>Occupation</div>
-      <div>Breed</div>
-      <div>Color</div>
-    </div>  
-    
   </div>
 </template>
 
@@ -37,32 +44,34 @@ export default {
     viewCatDetails() {
       this.$router.push("/catdetails");
     },
-    
+    getImageURL(pic) {
+      return require('../Assets/CatUsers/' + pic);
+    },
   },
   created() {
-      const catId = this.$store.state.catId;
-      
-      catService.getCatDetails(catId).then((response) => {
-        console.log(response)
-        console.log(response.data)
-        
+    const catId = this.$store.state.catId;
+
+    catService
+      .getCatDetails(catId)
+      .then((response) => {
+        console.log(response);
+        console.log(response.data);
+
         this.catsArray = response.data;
-      }).catch(error =>{
-        console.log(error.statusMsg)
+      })
+      .catch((error) => {
+        console.log(error.statusMsg);
       });
-    },
+  },
 };
 </script>
 
 
 
 <style>
-
-
 .image {
   display: block;
   width: 100%;
   height: auto;
 }
-
 </style>
