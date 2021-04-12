8612b42d06ca4bf6767403731be612ce4998d9ec
Student <student@Techs-MacBook-Air.local>
2021-04-12 13:23:03 -0400

    Updated Css

--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="nav" class="link">
-    <router-link class="link1" style="text-decoration: none;" v-bind:to="{ name: 'home' }"><a>HOME</a></router-link>
-    <router-link class="link2" style="text-decoration: none;" v-bind:to="{ name: 'cats' }"><a>CAT LIST</a></router-link>
-    <router-link class="link3" style="text-decoration: none;" v-bind:to="{ name: 'account' }" v-if="$store.state.token == ''"><a>ACCOUNT</a></router-link> 
-    <router-link class="link3" style="text-decoration: none;" v-bind:to="{ name: 'account' }" v-if="$store.state.token != ''"><a>ACCOUNT</a></router-link>
-    <router-link class="link4" style="text-decoration: none;" v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''"><a>LOGIN</a></router-link>
-    <router-link class="link4" style="text-decoration: none;" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"><a>LOGOUT</a></router-link>
-  <router-view></router-view> 
-  </div>
-  
+  <div class= "headCont">
+    <div class="logo">
+      <img src="../Assets/TabbyPro_Logo_250_100.png">
+    </div>
+    <div id="nav" class="link">
+      <router-link class="link1" style="text-decoration: none;" v-bind:to="{ name: 'home' }"><a>HOME</a></router-link>
+      <router-link class="link2" style="text-decoration: none;" v-bind:to="{ name: 'cats' }"><a>CAT LIST</a></router-link>
+      <router-link class="link3" style="text-decoration: none;" v-bind:to="{ name: 'account' }" v-if="$store.state.token == ''"><a>ACCOUNT</a></router-link> 
+      <router-link class="link3" style="text-decoration: none;" v-bind:to="{ name: 'account' }" v-if="$store.state.token != ''"><a>ACCOUNT</a></router-link>
+      <router-link class="link4" style="text-decoration: none;" v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''"><a>LOGIN</a></router-link>
+      <router-link class="link4" style="text-decoration: none;" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"><a>LOGOUT</a></router-link>
+    </div>
+</div>
   
 </template>
 
@@ -18,24 +21,39 @@ export default {};
 
 <style>
 
+.headCont{
+  display: flex;
+  flex-flow: row wrap;
+
+}
+.logo{
+  justify-content: start;
+  margin: auto;
+}
+
 .link {
   font-family: "Work Sans",sans-serif;
   font-size: 1.5em;
-  text-align: right;
-  margin: 1em 0em 10em 0em;
+  margin: auto auto auto auto;
+
+  justify-content: flex-end;
 }
 
 .link1 {
-  margin: 1em;
+  justify-content: center;
+  margin: 10px;
 }
 .link2 {
-  margin: 1em;
+  justify-content: center;
+  margin: 10px;
 }
 .link3 {
-  margin: 1em;
+  justify-content: center;
+  margin: 10px;
 }
 .link4 {
-  margin: 1em;
+  justify-content: center;
+  margin: 10px;
 }
 
 .link > a {
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,19 +1,9 @@
 <template>
   <div class="bgimg">
-  <div class="topleft">
-    <h2>Tabby Pro Cat Agency</h2>
+    <video autoplay muted loop id="myVideo">
+    <source src="../Assets/computer_cat.mp4" type="video/mp4">
+    </video>
   </div>
-  <div class="middle">
-    <h1>COMING SOON</h1>
-    <hr>
-    <p id="demo" style="font-size:35px"></p>
-  </div>
-  <div>
-  <video autoplay muted loop id="myVideo">
-  <source src="../Assets/computer_cat.mp4" type="video/mp4">
-  </video>
-  </div>
-</div>
 </template>
 
 <script>
@@ -21,38 +11,11 @@ export default {
   name: "home"
 };
 
-// Set the date we're counting down to
-var countDownDate = new Date("April 16, 2021 9:30:00").getTime();
-
-// Update the count down every 1 second
-var countdownfunction = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-  
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-  
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(countdownfunction);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
 </script>
 
 <style>
+
+
 /* Style the video: 100% width and height to cover the entire window */
 #myVideo {
   width: 99.67vw;
@@ -61,5 +24,8 @@ var countdownfunction = setInterval(function() {
     z-index: -100;
     margin-left: -50%;
     margin-right: -50%;
+}
+.bgimg {
+  text-align: center;
 }
 </style>
