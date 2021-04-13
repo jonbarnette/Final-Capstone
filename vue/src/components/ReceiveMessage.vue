414f66efcc4677133311c650d1331a28705d539e
Joeseph Brass <xenios239@gmail.com>
2021-04-13 11:03:22 -0400

    clean up

--- vue/src/components/ReceiveMessage.vue
+++ vue/src/components/ReceiveMessage.vue
@@ -56,33 +56,4 @@ export default {
 
 <style scoped>
 
-button[type="submit"] {
-  background-color: #d6dae6;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  width: 7.313%;
-  height: 2%;
-  position:absolute;
-  top: 2em;
-  right: 5em;
-}
-button[type="submit"]:hover {
-  background-color: #ff0000;
-  color: #163DA1;
-}
-.detailImage {
-  display: block;
-  width: 300px !important;
-  height: 250px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 1em;
-  border-radius: 50%;
-}
-* {
-  display:flex;
-  flex-direction: column;
-}
 </style>
