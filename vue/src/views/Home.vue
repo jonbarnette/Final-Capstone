3d4fe9c37acacf983751fc8fe79845029bc2b692
Charlie Nguyen <cnguyen1286@gmail.comm>
2021-04-14 17:11:21 -0400

    some front page styling

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,31 +1,97 @@
 <template>
   <div class="bgimg">
+    <div class="frontpageCats">
+      <div class="cats">
+        <img src="../Assets/CatUsers/HomepageCat.jpg" />
+        </div>
+      <p id="work1">Hi I'm looking for work!</p>
+      <div class="cats">
+        <img src="../Assets/CatUsers/AlleyCat.jpg" />
+        
+      </div>
+      <p id="work2">Do you have a position open?</p>
+    </div>
     <video autoplay muted loop id="myVideo">
-    <source src="../Assets/computer_cat.mp4" type="video/mp4">
+      <source src="../Assets/computer_cat.mp4" type="video/mp4" />
     </video>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
 };
-
 </script>
 
 <style>
-
-
 /* Style the video: 100% width and height to cover the entire window */
 #myVideo {
   width: 99.67vw;
-    height: 600px;
-    object-fit: cover;
-    z-index: -100;
-    margin-left: -50%;
-    margin-right: -50%;
+  height: 600px;
+  object-fit: cover;
+  z-index: -100;
+  margin-left: -50%;
+  margin-right: -50%;
 }
 .bgimg {
   text-align: center;
 }
+
+.frontpageCats {
+  display: flex;
+  
+  position: relative;
+  background-color: transparent;
+
+
+  
+}
+
+.cats {
+  flex: 0 1 30%;
+  align-items: space-around;
+  justify-content: center;
+  align-content: center;
+  margin: 10px;
+  box-shadow: 0px 8px 8px 0px grey;
+  transition: transform 1.2s ease 0s;
+  border: 5px solid black;
+  
+}
+
+
+
+.cats:hover {
+transition-delay: .1s;
+ transform: scale(1.02);
+}
+
+#work1 {
+  position: absolute;
+  top: 25%;
+  left: 45%;
+  z-index: 100;
+  color:white;
+  background-color: transparent;
+  font-size: 24px;
+  font-weight: bold;
+  transform: translate(-50% ,-50%);
+  
+  
+}
+#work2 {
+  position: absolute;
+  top: 40%;
+  right: 1%;
+  z-index: 100;
+  color:white;
+  background-color: transparent;
+  font-size: 24px;
+  font-weight: bold;
+  transform: translate(-50% ,-50%);
+  
+  
+}
+
+
 </style>
